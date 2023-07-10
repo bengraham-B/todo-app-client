5c8d4b21fb935b0383b40a00719907c501e9ae37
Ben Graham <grahamben7@gmail.com>
2023-07-10 20:33:32 +0200

    Added external url

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,7 +6,7 @@ export default function Login() {
     const [password, setPassword] = useState()
     const [error, setError] = useState()
 
-    const url = "https://todo-app-server-eta.vercel.app/"
+    const url = "https://todo-app-server-eta.vercel.app"
 
     const handleSignup = async () => {
         setError(null)
